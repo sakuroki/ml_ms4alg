efc83e6345686553179e8d636ed52c1b814ada01
Satoshi Kuroki <sasakida@gmail.com>
2020-12-09 20:31:25 +0900

    Update mountainsort4.py
    Enable new parameter isocut_threshold (this can be unnecessary??)

--- ml_ms4alg/mountainsort4.py
+++ ml_ms4alg/mountainsort4.py
@@ -1,18 +1,15 @@
 from .ms4alg import MountainSort4
-import os
-import shutil
 import tempfile
+import shutil
 import numpy as np
 import multiprocessing
-import spikeextractors as se
+from .plug_spikeextractors import _NumpySortingExtractor
 
-def mountainsort4(*,recording,detect_sign,clip_size=50,adjacency_radius=-1,detect_threshold=3,detect_interval=10,
-                  num_workers=None,verbose=True):
+def mountainsort4(*,recording,detect_sign,clip_size=50,adjacency_radius=-1,detect_threshold=3,detect_interval=10,num_workers=None,isocut_threshold=1):
   if num_workers is None:
     num_workers=int((multiprocessing.cpu_count()+1)/2)
 
-  if verbose:
-    print('Using {} workers.'.format(num_workers))
+  print('Using {} workers.'.format(num_workers))
 
   MS4=MountainSort4()
   MS4.setRecording(recording)
@@ -24,36 +21,33 @@ def mountainsort4(*,recording,detect_sign,clip_size=50,adjacency_radius=-1,detec
     detect_sign=detect_sign,
     detect_interval=detect_interval,
     detect_threshold=detect_threshold,
-    verbose=verbose
+    isocut_threshold=isocut_threshold
   )
-  tmpdir = tempfile.mkdtemp(dir=os.environ.get('TEMPDIR','/tmp'))
+  tmpdir = tempfile.mkdtemp()
   MS4.setNumWorkers(num_workers)
-  if verbose:
-    print('Using tmpdir: '+tmpdir)
+  print('Using tmpdir: '+tmpdir)
   MS4.setTemporaryDirectory(tmpdir)
   try:
     MS4.sort()
   except:
-    if verbose:
-      print('Cleaning tmpdir:: '+tmpdir)
+    print('Cleaning tmpdir:: '+tmpdir)
     shutil.rmtree(tmpdir)
     raise
-  if verbose:
-    print('Cleaning tmpdir::::: '+tmpdir)
+  print('Cleaning tmpdir::::: '+tmpdir)
   shutil.rmtree(tmpdir)
   times,labels,channels=MS4.eventTimesLabelsChannels()
-  output=se.NumpySortingExtractor()
-  output.set_times_labels(times=times,labels=labels)
+  output=_NumpySortingExtractor()
+  output.setTimesLabels(times=times,labels=labels)
   return output
 
 
 def _get_geom_from_recording(recording):
-  channel_ids=recording.get_channel_ids()
+  channel_ids=recording.getChannelIds()
   M=len(channel_ids)
-  location0=recording.get_channel_property(channel_ids[0],'location')
+  location0=recording.getChannelProperty(channel_ids[0],'location')
   nd=len(location0)
   geom=np.zeros((M,nd))
   for i in range(M):
-    location_i=recording.get_channel_property(channel_ids[i],'location')
+    location_i=recording.getChannelProperty(channel_ids[i],'location')
     geom[i,:]=location_i
   return geom
